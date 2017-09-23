0af641f0e4560a18e75328062a0237704f9316bb
fangjin <836473946@qq.com>
2017-09-23 23:46:12 +0800

    init

---
+++ admin/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import domaim.User;
+
+public class Main {
+    public static void main(String[] args){
+        User user = new User();
+        user.setId(1323L);
+        user.setName("dfsdf");
+        System.out.println(user);
+    }
+}
---
+++ core/src/main/java/domaim/User.java
@@ -0,0 +1,11 @@
+package domaim;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Long id;
+
+    private String name;
+}
